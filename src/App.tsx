ced12e45d85a52b27cd41f55d872eaaf8b5af6af
moneyandjelly <rmsdldy12@gmail.com>
2025-01-13 12:51:35 +0900

    feat: [이근] 게시글 작성 성공 및 실패 알림 메세지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 // 페이지 컴포넌트
 import LoginPage from "@pages/login/index";
 import BoardListPage from "@pages/board/index";
-import RegisterBoard from '@/pages/board/components/RegisterBoard/RegisterBoard';
+import RegisterBoard from "@/pages/board/components/RegisterBoard/RegisterBoard";
 // 리다이렉트
 import KakaoRedirect from "@pages/redirect/kakaologin/index";
 
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer />
       <Routes>
         <Route path="/login" element={<LoginPage />}></Route>
         <Route
--- src/pages/board/components/RegisterBoard/RegisterBoard.tsx
+++ src/pages/board/components/RegisterBoard/RegisterBoard.tsx
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
 import "./RegisterBoard.scss";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { requestRegisterPost } from "./../../../../apis/api/Board/requestRegisterPost";
+
 const RegisterBoard: React.FC = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const navigate = useNavigate();
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
     try {
-      const userToken = localStorage.getItem('userToken');
+      const userToken = localStorage.getItem("userToken");
       const data = await requestRegisterPost(title, content, userToken);
       if (data) {
-        console.log(data)
+        toast.success("등록이 완료되었습니다", {
+          position: "top-right",
+          autoClose: 3000,
+        });
+        navigate("/board/list");
       }
     } catch (error) {
       console.error("Error:", error);
+      toast.error("등록에 실패했습니다");
     }
   };
 
@@ -30,7 +41,7 @@ const RegisterBoard: React.FC = () => {
             onChange={(e) => setTitle(e.target.value)}
             required
           />
-        </div>  
+        </div>
         <div className="form-group">
           <label htmlFor="content">내용</label>
           <textarea
