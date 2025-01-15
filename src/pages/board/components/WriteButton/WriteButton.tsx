cb3c091b5136f45fa5f1950f4f32f94a21b09392
moneyandjelly <rmsdldy12@gmail.com>
2025-01-15 14:12:10 +0900

    feat: [이근] 글쓰기 버튼 클릭 시 유저 토큰 시간 검사

--- src/pages/board/components/WriteButton/WriteButton.tsx
+++ src/pages/board/components/WriteButton/WriteButton.tsx
@@ -3,8 +3,25 @@ import { useNavigate } from "react-router-dom";
 
 function WriteButton() {
   const navigate = useNavigate();
+  const getLocalStorageItem = (key) => {
+    const itemStr = localStorage.getItem(key);
+    if (!itemStr) {
+      console.log("유저 토큰 없음");
+      return navigate("/login");
+    }
+
+    const item = JSON.parse(itemStr);
+    const now = new Date();
+
+    if (now.getTime() > item.expireTime) {
+      console.log("유저 토큰 시간 만료");
+      localStorage.removeItem(key);
+      return navigate("/login");
+    }
+    return navigate("/board/register");
+  };
   const handleWriteClick = () => {
-    navigate("/board/register");
+    getLocalStorageItem("userToken");
   };
   return (
     <button onClick={handleWriteClick} className="write-button">
