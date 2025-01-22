6c0ecb7357ec1a42b04c58d3e6678cea5fa0e3a9
moneyandjelly <rmsdldy12@gmail.com>
2025-01-22 13:32:20 +0900

    feat: [이근] AWS 배포를 위한 환경설정

--- src/pages/board/components/ReadBoard/ReadBoard.tsx
+++ src/pages/board/components/ReadBoard/ReadBoard.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import "./ReadBoard.scss";
-import { requestReadPost } from "../../../../apis/api/Board/requestReadPost";
-import { requestDeletePost } from "../../../../apis/api/Board/requestDeletePost";
+import { requestReadPost } from "@apis/api/Board/requestReadPost";
+import { requestDeletePost } from "apis/api/Board/requestDeletePost";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../../store/store";
 
