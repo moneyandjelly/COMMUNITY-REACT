773a7810fb3209d1cd35238ade213f54081e3cec
moneyandjelly <rmsdldy12@gmail.com>
2025-02-05 13:34:38 +0900

    feat: [이근] 게시글 삭제 요청할 때 본인의 정보를 포함합니다.

--- src/apis/api/Board/requestDeletePost.js
+++ src/apis/api/Board/requestDeletePost.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 import { BASE_URL } from "../../../constants";
 
-export const requestDeletePost = async (boardId, userToken) => {
+export const requestDeletePost = async (
+  boardId,
+  userToken,
+  accountProfileId
+) => {
   try {
     const response = await axios.delete(`${BASE_URL}/board/${boardId}`, {
       data: {
         userToken: userToken,
+        accountProfileId: accountProfileId,
       },
     });
     return response.data;
